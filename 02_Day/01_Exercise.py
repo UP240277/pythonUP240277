d80e3fc04bcdd26e88ba8754125550c8f7d64236
UP240277 <up240277@alumnos.upa.edu.mx>
2025-03-02 11:18:47 -0600

    Python

--- 02_Day/01_Exercise.py
+++ 02_Day/01_Exercise.py
@@ -1,12 +1,34 @@
 #Día 2: Programación en python
+
+#3 Variable llamada nombre
 nombre="Zoeth"
+
+#4 Variable llamada apellido
 apellido="Rodríguez"
+
+#5 Variable llamada Nombre Completo
 nombreCompleto=nombre+" "+apellido
+
+#6 Variable llamada país
 país="México"
+
+#7 Variable llamada ciudad
 ciudad="Aguascalientes"
+
+#8 Variable llamada edad
 edad=18
+
+#9 Variable llamada año
 año=2025
+
+#10 Variable llamada is_married
 is_married=False
+
+#11 Variable llamada is_true
 is_true=True
+
+#12 Variable llamada is_light_on
 is_light_on=False
+
+#13 Variables en una línea
 num1, num2=1, 2
--- 02_Day/02_Exercise.py
+++ 02_Day/02_Exercise.py
@@ -1,3 +1,4 @@
+#1 Checar el tipo de dato usando la función 'type'
 nombre="Zoeth"
 apellido="Rodríguez"
 nombre_Completo=nombre+" "+apellido
@@ -22,30 +23,50 @@ print(type(is_true))
 print(type(is_light_on))
 print(type(num1, num2))
 
+#2 Función 'len' y encontrar la longitud de la variable nombre
 print(len(nombre))
+
+#3 Comparar la longitud de nombre con apellido
 print(len(apellido))
 
+#4 Declarar 5 como número 1 y 4 como número 2
 num_one=5
 num_two=4
+
+#5 Sumar número uno y dos
 total=num_one+num_two
+
+#6 Restar número uno y dos
 diff=num_one-num_two
+
+#7 Multiplicar número uno y dos
 product=num_one*num_two
+
+#8 Dividir número uno y dos
 division=num_one/num_two
+
+#9 Sacar el módulo de uno entre dos
 remainder=num_two%num_one
+
+#10Calcular la potencia de número uno a número dos
 exp=num_one**num_two
+
+#11 Hallar floor division de número uno entre número dos
 floor_division=num_one//num_two
 
 import math
-#Área de un circulo
+#12 Área de un circulo
 radius= float(input("Introduce el radio del circulo: "))
 area_of_circle=math.pi*radius**2
 print("El área del círculo es de: " , area_of_circle)
 circum_of_circle=math.pi*2*radius
 print("La circunferencia del círculo es de: " , circum_of_circle)
 
+#13 Usar la función 'input'
 nombre=input("Ingresa tu nombre: ")
 apellido=input("Ingresa tu apellido: ")
 país=input("Ingresa tu país: ")
 edad=input("Ingresa tu edad: ")
 
+#14 Correr 'help(keywords)'
 help('keywords')
